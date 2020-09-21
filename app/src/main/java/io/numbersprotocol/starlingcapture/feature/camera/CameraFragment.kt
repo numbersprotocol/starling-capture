05be1d7239c2fb5dba7ce5ad2c534130ca5c9585
Sean Wu <seanwu1105@gmail.com>
2020-09-21 14:40:26 +0800

    Implement basic video recording.

--- app/src/main/java/io/numbersprotocol/starlingcapture/feature/camera/CameraFragment.kt
+++ app/src/main/java/io/numbersprotocol/starlingcapture/feature/camera/CameraFragment.kt
@@ -1,5 +1,6 @@
 package io.numbersprotocol.starlingcapture.feature.camera
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.otaliastudios.cameraview.CameraListener
 import com.otaliastudios.cameraview.PictureResult
+import com.otaliastudios.cameraview.VideoResult
+import com.otaliastudios.cameraview.controls.Mode
 import io.numbersprotocol.starlingcapture.R
 import io.numbersprotocol.starlingcapture.databinding.FragmentCameraBinding
 import io.numbersprotocol.starlingcapture.util.MimeType
@@ -19,6 +22,14 @@ import java.io.File
 class CameraFragment : Fragment() {
 
     private val cameraViewModel: CameraViewModel by viewModel()
+    private lateinit var videoFile: File
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Support portrait orientation only.
+        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +46,7 @@ class CameraFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initializeCameraView()
         navBackButton.setOnClickListener { findNavController().navigateUp() }
+        cameraViewModel.cameraMode.observe(viewLifecycleOwner) { onModeUpdated(it) }
     }
 
     private fun initializeCameraView() {
@@ -43,19 +55,58 @@ class CameraFragment : Fragment() {
             override fun onPictureTaken(result: PictureResult) {
                 super.onPictureTaken(result)
                 val cachedMediaFile = createCachedMediaFile(MimeType.JPEG)
-                result.toFile(cachedMediaFile) { cameraViewModel.storeImage(it!!) }
+                result.toFile(cachedMediaFile) { cameraViewModel.storeMedia(it!!, MimeType.JPEG) }
                 snack(R.string.media_captured, anchorView = captureButton)
             }
+
+            override fun onVideoTaken(result: VideoResult) {
+                super.onVideoTaken(result)
+                cameraViewModel.storeMedia(result.file, MimeType.MP4)
+                snack(R.string.media_captured, anchorView = captureButton)
+            }
+
+            override fun onVideoRecordingStart() {
+                super.onVideoRecordingStart()
+                cameraViewModel.isTakingVideo.value = true
+            }
+
+            override fun onVideoRecordingEnd() {
+                super.onVideoRecordingEnd()
+                cameraViewModel.isTakingVideo.value = false
+            }
         })
-        captureButton.setOnClickListener { cameraView.takePicture() }
     }
 
-    private fun createCachedMediaFile(@Suppress("SameParameterValue") mimeType: MimeType): File {
+    private fun onModeUpdated(mode: Mode) {
+        cameraView.mode = mode
+        if (mode == Mode.PICTURE) captureButton.setOnClickListener { cameraView.takePicture() }
+        else captureButton.setOnClickListener { onRecordClicked() }
+    }
+
+    private fun onRecordClicked() {
+        if (cameraView.isTakingVideo) cameraView.stopVideo()
+        else {
+            videoFile = createCachedMediaFile(MimeType.MP4)
+            cameraView.takeVideo(videoFile)
+        }
+    }
+
+    private fun createCachedMediaFile(mimeType: MimeType): File {
         val timestamp = System.currentTimeMillis()
         return File.createTempFile(
             "$timestamp",
             ".${mimeType.extension}",
             requireContext().cacheDir
         )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        removeCachedVideoFile()
+        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+    }
+
+    private fun removeCachedVideoFile() {
+        if (videoFile.exists()) videoFile.delete()
     }
 }
--- app/src/main/java/io/numbersprotocol/starlingcapture/feature/camera/CameraViewModel.kt
+++ app/src/main/java/io/numbersprotocol/starlingcapture/feature/camera/CameraViewModel.kt
@@ -1,17 +1,35 @@
 package io.numbersprotocol.starlingcapture.feature.camera
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
+import com.otaliastudios.cameraview.controls.Mode
+import io.numbersprotocol.starlingcapture.R
 import io.numbersprotocol.starlingcapture.collector.ProofCollector
 import io.numbersprotocol.starlingcapture.util.MimeType
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import java.io.File
 
 class CameraViewModel(private val proofCollector: ProofCollector) : ViewModel() {
 
-    fun storeImage(cachedMediaFile: File) = viewModelScope.launch(Dispatchers.IO) {
-        proofCollector.storeAndCollect(cachedMediaFile, MimeType.JPEG)
-        if (cachedMediaFile.exists()) cachedMediaFile.delete()
+    val cameraMode = MutableLiveData(Mode.PICTURE)
+    val isTakingVideo = MutableLiveData(false)
+    val captureButtonIcon =
+        combine(cameraMode.asFlow(), isTakingVideo.asFlow()) { mode, isTakingVideo ->
+            when {
+                mode == Mode.PICTURE -> R.drawable.ic_capture
+                isTakingVideo -> R.drawable.ic_stop
+                else -> R.drawable.ic_fiber_manual_record
+            }
+        }.asLiveData(timeoutInMs = 0)
+
+    fun storeMedia(cachedMediaFile: File, mimeType: MimeType) =
+        viewModelScope.launch(Dispatchers.IO) {
+            proofCollector.storeAndCollect(cachedMediaFile, mimeType)
+            if (cachedMediaFile.exists()) cachedMediaFile.delete()
+        }
+
+    fun switchMode() {
+        cameraMode.value = if (cameraMode.value == Mode.PICTURE) Mode.VIDEO else Mode.PICTURE
     }
 }
