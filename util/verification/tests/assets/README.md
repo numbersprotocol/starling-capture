2e55d819f85ffa170225fa8618c4a5aa0734f570
Bofu Chen (bafu) <bofu@numbersprotocol.io>
2022-08-05 18:17:49 +0800

    test: re-arrange testing data locations
    Signed-off-by: Bofu Chen (bafu) <bofu@numbersprotocol.io>

---
+++ util/verification/tests/assets/README.md
@@ -0,0 +1,68 @@
+# Quick Start
+
+
+
+# Example Key Pairs
+
+SW key pair
+
+1. default public key
+
+```
+3059301306072a8648ce3d020106082a8648ce3d03010703420004813afd7f6ba95fdebeac7812c7ab5af9ca59547b2a73f5aa75accca4e4ad2eb6849d4948a9fcfb8c2b890ba0dfbe4463bbfe4ac163981a93517bfb34598fc850
+```
+
+2. default private key
+
+```
+308187020100301306072a8648ce3d020106082a8648ce3d030107046d306b02010104204ebe7c7bff6b0e8954c061cb360fa777821b65aade8cc4b64e4b8c95c0aafb44a14403420004813afd7f6ba95fdebeac7812c7ab5af9ca59547b2a73f5aa75accca4e4ad2eb6849d4948a9fcfb8c2b890ba0dfbe4463bbfe4ac163981a93517bfb34598fc850
+```
+
+Zion
+1. mnemonics
+
+```
+pet effort swear system breeze arrow proof green disorder retreat fox bargain
+```
+
+2. private key
+
+```
+7b465693570629061ab4b1ae1b1da3ebeec9ee675518131be591e94db187d235
+```
+
+3. wallet address
+
+```
+0x5f5AD77F4f924232a6E486216Ddefba8a732b96B
+```
+
+Session key pair
+
+1. Public key
+
+```
+3059301306072a8648ce3d020106082a8648ce3d030107034200045f4876d8ae65477b5985484ee9d6fd2a5a78b2bcebdaf090f7c9bb85c89f4de55f909e5663caac8cd6e958d8eff50a029ec4c7245aa1ea1716787c62229a9a34
+```
+
+2. Private key
+
+```
+308187020100301306072a8648ce3d020106082a8648ce3d030107046d306b020101042022619451d73a16a0c41c3507435948af61f1a8dbbb901d5b18a1cf2ff648cafea144034200045f4876d8ae65477b5985484ee9d6fd2a5a78b2bcebdaf090f7c9bb85c89f4de55f909e5663caac8cd6e958d8eff50a029ec4c7245aa1ea1716787c62229a9a34
+```
+
+Public key signature
+
+```
+d779896950561be2f2532758ba977d1a04280e98c86d7cde4b1b58f570ec4a6530c2c163b997ae3dcaa843e922f0c888e591adde46b84f1b20001ad68d47686b1b
+```
+
+# Example Assets
+
+1. HW: Exodus 1s
+1. Starling Capture: 1.8.0-master-debug
+1. Enable Zion: true
+1. Enable Session Signature: false
+1. Asset hash: `ede8c1b820a19e0a438ce28873501fafd33886a21cf4e3c1ffea24f523c857c4`
+1. AndroidOpenSSL verification: pass
+1. [Download link](https://app.asana.com/app/asana/-/get_asset?asset_id=1202279149900570&force_download)
--- util/verification/tests/test_verification.py
+++ util/verification/tests/test_verification.py
@@ -9,20 +9,35 @@ from verification import verify_ecdsa_with_sha256
 from verification import verify_ethereum_signature
 
 
-INFORMATION_FILENAME = './tests/assets/information.json'
-SIGNATURE_FILENAME = './tests/assets/signature.json'
-SIGNER_WALLET_ADDRESS = '0x5f5AD77F4f924232a6E486216Ddefba8a732b96B'
+Testing_captures = {
+    'sw_key_signature': {
+        'information_filename': './tests/assets/zion/9f9af13673a0ea1e608fdd4c6ce6c746dd03dd58d184705ea7d1e1ef175e5640/information.json',
+        'signature_filename': './tests/assets/zion/9f9af13673a0ea1e608fdd4c6ce6c746dd03dd58d184705ea7d1e1ef175e5640/signature.json',
+        'signer_wallet_address': '0x5f5AD77F4f924232a6E486216Ddefba8a732b96B'
+    },
+    'hw_key_signature': {
+        'information_filename': './tests/assets/zion/9f9af13673a0ea1e608fdd4c6ce6c746dd03dd58d184705ea7d1e1ef175e5640/information.json',
+        'signature_filename': './tests/assets/zion/9f9af13673a0ea1e608fdd4c6ce6c746dd03dd58d184705ea7d1e1ef175e5640/signature.json',
+        'signer_wallet_address': '0x5f5AD77F4f924232a6E486216Ddefba8a732b96B'
+    },
+    'hw_key_signature_classic': {
+        'information_filename': './tests/assets/zion-classic/ede8c1b820a19e0a438ce28873501fafd33886a21cf4e3c1ffea24f523c857c4/information.json',
+        'signature_filename': './tests/assets/zion-classic/ede8c1b820a19e0a438ce28873501fafd33886a21cf4e3c1ffea24f523c857c4/signature.json',
+        'signer_wallet_address': '0x5f5AD77F4f924232a6E486216Ddefba8a732b96B'
+    }
+}
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_sw_key_verification():
+    print('test_sw_key_verification')
 
+    capture = Testing_captures['sw_key_signature']
+    signatures = list(read_json_file(capture['signature_filename']))
 
-def test_verify_ecdsa_with_sha256():
-    signatures = list(read_json_file(SIGNATURE_FILENAME))
     for signature in signatures:
         if signature['provider'] == 'AndroidOpenSSL':
-            message = read_text_file(INFORMATION_FILENAME)
+            message = read_text_file(capture['information_filename'])
+            print(f'\tmessage: {message}, signature: {signature["signature"]}')
             assert verify_ecdsa_with_sha256(
                 message=message,
                 signature_hex=signature['signature'],
@@ -30,28 +45,32 @@ def test_verify_ecdsa_with_sha256():
             )
 
 
-def test_verify_ethereum_signature():
-    message = generate_sha256_from_filepath(INFORMATION_FILENAME)
-    signature = get_signature_from_information_file(SIGNATURE_FILENAME)
+def test_hw_key_verification():
+    print('test_hw_key_verification')
+    capture = Testing_captures['hw_key_signature']
+    message = generate_sha256_from_filepath(capture['information_filename'])
+    signature = get_signature_from_information_file(capture['signature_filename'])
+    print(f'\tmessage: {message}, signature: {signature}')
     recovered_wallet_address = verify_ethereum_signature(message, signature)
-    assert SIGNER_WALLET_ADDRESS == recovered_wallet_address
+    assert capture['signer_wallet_address'] == recovered_wallet_address
 
 
-def test_verify_legacy_zion_signature():
-    ASSET_SHA256='ede8c1b820a19e0a438ce28873501fafd33886a21cf4e3c1ffea24f523c857c4'
-    INFORMATION_FILENAME = f'./tests/assets/zion-legacy/{ASSET_SHA256}/information.json'
-    SIGNATURE_FILENAME = f'./tests/assets/zion-legacy/{ASSET_SHA256}/signature.json'
-    SIGNER_WALLET_ADDRESS = '0x5f5AD77F4f924232a6E486216Ddefba8a732b96B'
-
-    sha256sum = generate_sha256_from_filepath(INFORMATION_FILENAME)
+def test_hw_key_verification_classic():
+    print('test_hw_key_verification_classic')
+    capture = Testing_captures['hw_key_signature_classic']
+    sha256sum = generate_sha256_from_filepath(capture['information_filename'])
     message = hex_string_to_bytes(sha256sum)
-    signature = get_signature_from_information_file(SIGNATURE_FILENAME)
+    signature = get_signature_from_information_file(capture['signature_filename'])
+    print(f'\tmessage: {message}, signature: {signature}')
     recovered_wallet_address = verify_ethereum_signature(message, signature)
-    assert SIGNER_WALLET_ADDRESS == recovered_wallet_address
+    assert capture['signer_wallet_address'] == recovered_wallet_address
 
 
 def test_verify():
-    INFORMATION_FILENAME = f'./tests/assets/information.json'
-    SIGNATURE_FILENAME = f'./tests/assets/signature.json'
-    SIGNER_WALLET_ADDRESS = '0x5f5AD77F4f924232a6E486216Ddefba8a732b96B'
-    verify(INFORMATION_FILENAME, SIGNATURE_FILENAME, SIGNER_WALLET_ADDRESS)
+    print('test_verify')
+    capture = Testing_captures['hw_key_signature']
+    verify(
+        capture['information_filename'],
+        capture['signature_filename'],
+        capture['signer_wallet_address']
+    )
